f8b1e04f05ed67ad38e27e81e1e4834051465841
Tam Thai <hoangtam3062002@gmail.com>
2022-10-08 09:39:04 +0700

    weather show correct time

--- commands/weather.js
+++ commands/weather.js
@@ -12,8 +12,35 @@ module.exports = async function (msg, tokens) {
   temp /= 10;
   let weather = response.weather[0]["main"];
   let description = response.weather[0]["description"];
-  let time = new Date(response["dt"]).toISOString().slice(11, -1);
+  let time = timeConverter(response["dt"]);
   msg.channel.send(
     `Location: ${location}, Temp: ${temp}, weather: ${weather}, description: ${description}, Time: ${time}`
   );
 };
+
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time =
+    date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
+  return time;
+}
