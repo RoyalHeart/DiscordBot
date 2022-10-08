624a3bae189e0e15ffb427c949090531781047a4
Tam Thai <hoangtam3062002@gmail.com>
2022-10-08 09:38:47 +0700

    !yt can show 3 best fit search

--- commands/yt.js
+++ commands/yt.js
@@ -1,6 +1,14 @@
-module.exports = function (msg, tokens) {
-  let search_query = tokens.slice(0, tokens.length).join("+");
-  msg.channel.send(
-    "https://www.youtube.com/results?search_query=" + search_query
-  );
+const youtube_api_key = process.env.YOUTUBE_API_KEY;
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+module.exports = async function (msg, tokens) {
+  let searchQuery = tokens.join("%20");
+  let search_query = `https://youtube.googleapis.com/youtube/v3/search?q=${searchQuery}&key=${youtube_api_key}`;
+  let data = await (await fetch(search_query)).json();
+  let results = data["items"];
+  for (let i = 0; i < 3; i++) {
+    let videoId = results[i]["id"]["videoId"];
+    let videoUrl = `https://youtube.com/watch?v=${videoId}`;
+    msg.channel.send(videoUrl);
+  }
 };
